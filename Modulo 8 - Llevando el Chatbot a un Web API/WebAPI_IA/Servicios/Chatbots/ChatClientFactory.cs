94ceda12e4709032f5ce6d206aa8390b04f45093
Felipe Gavilán <gavilanch@hotmail.com>
2026-06-16 15:07:53 -0400

    Actualizando

---
+++ Modulo 8 - Llevando el Chatbot a un Web API/WebAPI_IA/Servicios/Chatbots/ChatClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.AI;
+
+namespace WebAPI_IA.Servicios.Chatbots
+{
+    public class ChatClientFactory(IConfiguration configuration, IServiceProvider sp) : IChatClientFactory
+    {
+        public IChatClient Crear()
+        {
+            var llaveOpenAI = configuration.GetValue<string>("OPENAI_LLAVE");
+            var modelo = configuration.GetValue<string>("OPENAI_MODELO");
+
+            var cliente = new OpenAI.Chat.ChatClient(modelo ?? "gpt-5.4-nano", llaveOpenAI).AsIChatClient();
+
+            return cliente.AsBuilder()
+            .UseFunctionInvocation(null, c =>
+            {
+                c.IncludeDetailedErrors = true;
+            })
+            .Build(sp);
+        }
+    }
+
+}
